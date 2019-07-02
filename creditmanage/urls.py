844ed119a0a549d88af2e53b609388d7d518c5ac
ambesh123 <ambesht0@gmail.com>
2019-07-03 02:01:29 +0530

    worked hard and added all facilites of balance transfer along with beutiful css and js

--- creditmanage/urls.py
+++ creditmanage/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
+from django.urls import path , re_path
 from . import views
 
 urlpatterns = [
     path('', views.home),
     path('users/' , views.users),
+    path('transfer/', views.transfer),
+    path('choose_sender/' , views.choose_sender),
+    path('choose_receiver/' , views.choose_receiver),
+    path('enter_amount/', views.enter_amount),
+    path('set_amount/', views.set_amount),
+    path('finalize/', views.finalize),
 ]
--- creditmanage/views.py
+++ creditmanage/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render , HttpResponse , redirect
 from .models import client
 
 def home(request):
@@ -7,3 +7,52 @@ def home(request):
 def users(request):
 	clients = client.objects.all()
 	return render(request , 'users.html' , {'clients' : clients})
+
+def choose_sender(request):
+	clients = client.objects.all()
+	return render(request , 'choose_sender.html' , {'clients' : clients})
+
+def choose_receiver(request):
+	sender = request.GET.get('sender')
+	clients = client.objects.exclude(uid = sender)
+	if sender == None:
+		return(HttpResponse("<h1>Invalid Request</h1>"))
+	request.session['sender'] = sender
+	return render(request, 'choose_receiver.html' , {'clients' : clients , 'sender' : sender})
+
+def enter_amount(request):
+	receiver = request.GET.get('receiver')
+	if receiver == None:
+		return(HttpResponse("<h1>Invalid Request</h1>"))
+	request.session['receiver'] = receiver
+	return render(request, 'enter_amount.html')
+
+def set_amount(request):
+	amount = request.GET.get('amount')
+	if amount == None:
+		return(HttpResponse("<h1>Invalid Request</h1>"))
+	request.session['amount'] = amount
+	return HttpResponse("<center><button class = 'btn btn-lg btn-danger' onclick = 'finalize()'>Confirm</button></center>")
+
+def transfer(request):
+	return render(request , 'transfer.html')
+
+def finalize(request):
+	sender_id = request.session['sender']
+	receiver_id = request.session['receiver']
+	amount = request.session['amount']
+
+	if sender_id == None or receiver_id == None or amount == None :
+		return HttpResponse("Bad Request")
+
+	sender = client.objects.get(uid = sender_id)
+	receiver = client.objects.get(uid = receiver_id)
+	amount = int(amount)
+
+	sender.deductMoney(amount)
+	receiver.addMoney(amount)
+
+	sender.save()
+	receiver.save()
+
+	return redirect('/')
